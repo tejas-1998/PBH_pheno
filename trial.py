3530d5cf67f039dac539fb48d8908af8c4844d16
tejas-1998 <128160818+tejas-1998@users.noreply.github.com>
2023-03-17 20:02:32 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,10 @@
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+from scipy import interpolate
+import time
+
+a = np.sin(pi/2)
+b = np.cos(pi/2)
+print(a+b)
